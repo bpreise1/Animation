f1ab85b41d84eeb01b0fcab9b0ff3665c653aed5
bpreise1 <47012838+bpreise1@users.noreply.github.com>
2021-10-05 23:04:40 -0400

    Simple Animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,36 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.Timer;
 
 public class MyGdxGame extends ApplicationAdapter {
+
 	SpriteBatch batch;
-	private TextureAtlas crocImg;
-	private Sprite croc;
-	private int currentFrame = 1;
-	private String currentAtlasKey = new String("crocodile_round");
+	private TextureAtlas animalAtlas;
+	private Animation<TextureRegion> animation;
+	private float elapsedTime = 0f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//crocImg = new Texture("badlogic.jpg");
+
+		animalAtlas = new TextureAtlas(Gdx.files.internal("Animals.atlas"));
+		animation = new Animation<TextureRegion>(1f, animalAtlas.getRegions());
 	}
 
 	@Override
 	public void render () {
+		elapsedTime += Gdx.graphics.getDeltaTime();
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 0,0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//img.dispose();
+		animalAtlas.dispose();
 	}
 }
