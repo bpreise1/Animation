f063c18cb7ac3716c0415b2b75cb97c5229aef39
bpreise1 <47012838+bpreise1@users.noreply.github.com>
2021-10-05 23:36:33 -0400

    Basketball

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,19 +2,16 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.Timer;
 
 public class MyGdxGame extends ApplicationAdapter {
 
 	SpriteBatch batch;
-	private TextureAtlas animalAtlas;
+	private TextureAtlas basketballAtlas;
 	private Animation<TextureRegion> animation;
 	private float elapsedTime = 0f;
 	
@@ -22,22 +19,22 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 
-		animalAtlas = new TextureAtlas(Gdx.files.internal("Animals.atlas"));
-		animation = new Animation<TextureRegion>(1f, animalAtlas.getRegions());
+		basketballAtlas = new TextureAtlas(Gdx.files.internal("Basketball.atlas"));
+		animation = new Animation<TextureRegion>(.075f, basketballAtlas.getRegions());
 	}
 
 	@Override
 	public void render () {
 		elapsedTime += Gdx.graphics.getDeltaTime();
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(animation.getKeyFrame(elapsedTime, true), 0,0);
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 50,50, 500, 500);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		animalAtlas.dispose();
+		basketballAtlas.dispose();
 	}
 }
